2839205b790536faad0f72133185764cb0b4daed
brownae <aebrown9@gmail.com>
2017-05-20 16:34:16 -0700

    Initial commit to add index.php and a nav bar to start with
    Nav bar needs to be updated to color scheme.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include('includes/header.php');?>
+<img class="top-img" src="img/truffle-old-fashioned-cocktail-canon-seattle.jpg" alt="truffle old fashioned cocktail canon seattle">
+
+
+
+
+
+<?php include('includes/footer.php');?>
