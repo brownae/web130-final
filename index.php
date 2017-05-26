41de6f65a3f627d6845cc40b4ad6658bc36c2d18
brownae <aebrown9@gmail.com>
2017-05-26 08:36:25 -0700

    Improves seo by updating a few alt fields.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include('includes/header.php');?>
 <img class="top-img" src="img/truffle-old-fashioned-cocktail-canon-seattle.jpg" alt="truffle old fashioned cocktail canon seattle">
 <div class="logo">
-    <img id='main-logo' src="img/canon-logo-white.svg" alt="Canon logo">
+    <img id='main-logo' src="img/canon-logo-white.svg" alt="Canon whiskey and bitters emporium">
 </div>
 
 
