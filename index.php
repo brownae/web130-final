243f613c864f912203ab3cd5c385252f22c1b753
brownae <aebrown9@gmail.com>
2017-06-01 13:01:27 -0700

    Imports fonts, arranges sass files and installs cta's on home page

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
 <?php include('includes/header.php');?>
+
 <img class="top-img" src="img/truffle-old-fashioned-cocktail-canon-seattle.jpg" alt="truffle old fashioned cocktail canon seattle">
-<div class="logo">
-    <img id='main-logo' src="img/canon-logo-white.svg" alt="Canon whiskey and bitters emporium">
-</div>
+
+<main id="homePage">
+    <div class="logo">
+        <h1>Canon whiskey and bitters emporium</h1>
+        <img id='main-logo' src="img/canon-logo-white.svg" alt="Canon whiskey and bitters emporium">
+
+    </div>
+    <div class="info">
+        <p>Open 5pm-2am daily.</p>
+        <p>Closed July 4th, Thanksgiving, Christmas Day, and New Year's Day</p>
+        <address>928 12th Ave, Seattle, WA</address>
+    </div>
+    <div class="cta cta1">
+        <a href="#" ><h2>Food</h2></a>
+        <img src="img/cta-food.png" alt="Food menu for Canon restaurant">
+    </div>
+    <div class="cta cta2">
+        <a href="#" ><h2>Cocktails</h2></a>
+        <img src="img/cta-cocktails.png" alt="Cocktail menu for Canon restaurant">
+    </div>
+    <div class="cta cta3">
+        <a href="#" ><h2>Bottle Library</h2></a>
+        <img src="img/cta-bottle-library.png" alt="Bottle Library for Canon restaurant">
+    </div>
+    <div class="cta cta4">
+        <a href="#" ><h2>Book</h2></a>
+        <img src="img/cta-book.png" alt="Book for Canon restaurant">
+    </div>
 
 
 
 
-
+</main>
 <?php include('includes/footer.php');?>
